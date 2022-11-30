27929a9b97f3e271eb64382985f7a380a1ee0f7c
Mihály Áron Németh <mihaly.nemeth@stud.uni-corvinus.hu>
2022-11-30 10:40:37 +0100

    felcsesz

---
+++ hajos_gyak/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hajos_gyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route ("questions/all")]
+        public IActionResult adjad()
+        {
+            Models.HajosContext hajosContext = new Models.HajosContext();
+            var list = from x in hajosContext.Questions
+                       select x.Question1;
+            return new JsonResult(list);
+        }
+    }
+}
---
+++ hajos_gyak/Controllers/TesztController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hajos_gyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+            return Ok(pontosIdő);
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return Ok(szoveg.ToUpper());
+        }
+    }
+}
